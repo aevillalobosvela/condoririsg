acc2ebecdd3408f894d8b829256f9e4bbf01ddce
Alvaro Villalobos Vela <vivealed@gmail.com>
2026-02-23 17:31:14 -0400

    refactor(botonesventa): se cambia el estilo de los botones de generacion de reportes de inventario/ventas para verse similar a ventas

--- app/Views/inventarios/index.php
+++ app/Views/inventarios/index.php
@@ -350,46 +350,46 @@
               <i class="ri-calendar-line me-1"></i> Hoy
             </button>
           </div>
+          <!-- Grupo Excel -->
           <div class="col-md-2">
-            <a href="<?= base_url('inventarios/exportarExcelVentas?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=deposito_contado&sucursal_id=4') ?>"
-              class="btn btn-success w-100">
-              <i class="ri-file-excel-2-line me-1"></i> Excel Contado
-            </a>
+            <div class="btn-group w-100">
+              <button type="button" class="btn btn-success dropdown-toggle" data-bs-toggle="dropdown">
+                <i class="ri-file-excel-2-line"></i> Excel
+              </button>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="<?= base_url('inventarios/exportarExcelVentas?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=contado&sucursal_id=4') ?>">
+                  <i class="ri-hand-coin-line text-primary me-2"></i>Contado
+                </a></li>
+                <li><a class="dropdown-item" href="<?= base_url('inventarios/exportarExcelVentas?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=credito&sucursal_id=4') ?>">
+                  <i class="ri-wallet-3-line text-warning me-2"></i>Crédito
+                </a></li>
+                <li><a class="dropdown-item" href="<?= base_url('inventarios/exportarExcelVentas?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=general&sucursal_id=4') ?>">
+                  <i class="ri-file-list-line text-info me-2"></i>General
+                </a></li>
+              </ul>
+            </div>
           </div>
+          <!-- Grupo PDF -->
           <div class="col-md-2">
-            <a href="<?= base_url('inventarios/cierre/rango?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=contado') ?>"
-              class="btn btn-outline-primary w-100" target="_blank">
-              <i class="ri-file-pdf-line me-1"></i> Contado Deposito
-            </a>
-          </div>
-          <div class="col-md-2">
-            <a href="<?= base_url('inventarios/exportarExcelVentas?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=credito&sucursal_id=4') ?>"
-              class="btn btn-success w-100">
-              <i class="ri-file-excel-2-line me-1"></i> Excel Crédito
-            </a>
-          </div>
-          <div class="col-md-2">
-            <a href="<?= base_url('inventarios/cierre/rango?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=credito') ?>"
-              class="btn btn-outline-warning w-100" target="_blank">
-              <i class="ri-file-pdf-line me-1"></i> Crédito
-            </a>
-          </div> 
-          <div class="col-md-2">
-            <a href="<?= base_url('inventarios/exportarExcelVentas?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=general&sucursal_id=4') ?>"
-              class="btn btn-success w-100">
-              <i class="ri-file-excel-2-line me-1"></i> Excel General
-            </a>
-          </div>
-          <div class="col-md-2">
-            <a href="<?= base_url('inventarios/cierre/rango?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=general') ?>"
-              class="btn btn-info w-100" target="_blank">
-              <i class="ri-file-pdf-line me-1"></i> Reporte General
-            </a>
-          </div>
-          <div class="col-md-2">
-             <a href="<?= base_url('inventarios/cierre/rango?fecha_inicio=' . date('Y-m-d') . '&fecha_fin=' . date('Y-m-d') . '&tipo=deposito_contado') ?>" class="btn btn-info btn-sm text-white" target="_blank">
-        <i class="ri-file-pdf-line align-bottom me-1"></i> Contado
-      </a>
+            <div class="btn-group w-100">
+              <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown">
+                <i class="ri-file-pdf-line"></i> PDF
+              </button>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="<?= base_url('inventarios/cierre/rango?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=deposito_contado') ?>" target="_blank">
+                  <i class="ri-bank-line text-primary me-2"></i>Contado
+                </a></li>
+                <li><a class="dropdown-item" href="<?= base_url('inventarios/cierre/rango?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=contado') ?>" target="_blank">
+                  <i class="ri-hand-coin-line text-primary me-2"></i>Contado Depósito
+                </a></li>
+                <li><a class="dropdown-item" href="<?= base_url('inventarios/cierre/rango?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=credito') ?>" target="_blank">
+                  <i class="ri-wallet-3-line text-warning me-2"></i>Crédito
+                </a></li>
+                <li><a class="dropdown-item" href="<?= base_url('inventarios/cierre/rango?fecha_inicio=' . urlencode($fecha_desde) . '&fecha_fin=' . urlencode($fecha_hasta) . '&tipo=general') ?>" target="_blank">
+                  <i class="ri-file-list-line text-info me-2"></i>General
+                </a></li>
+              </ul>
+            </div>
           </div>
         </form>
       </div>
